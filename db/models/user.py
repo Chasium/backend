7fb40b854ed00848d3c9fc10ca02821f30eed5f7
汪静雅 <wjy_20020731@163.com>
2023-01-09 16:57:32 +0800

    修改可上传图片大小上限

--- db/models/user.py
+++ db/models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Text
 from db import db
 import random
 
@@ -9,7 +9,7 @@ class UserData(db.Model):
     id = Column(Integer, primary_key=True)
     name = Column(String(50), unique=True)
     password = Column(String(120), unique=False)
-    img = Column(String(5000))  # base64编码后的图像
+    img = Column(Text(65535))  # base64编码后的图像
     nickname = Column(String(50))
 
     def __init__(self, name, password):
@@ -20,9 +20,12 @@ class UserData(db.Model):
 
     def getId(self):
         return self.id
+
     def getName(self):
         return self.name
+
     def modifyImage(self, img: str):
         self.img = img
+
     def modifyNickname(self, nickname: str):
         self.nickname = nickname
